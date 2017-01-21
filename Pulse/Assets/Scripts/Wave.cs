87a5360bf42079e6fa2749472c602e4316eeca8a
PKJamoo <lkavanagh010@gmail.com>
2017-01-21 11:44:25 -0800

    the new shit

---
+++ Pulse/Assets/Scripts/Wave.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    public int segments;
+    public float radius;
+    LineRenderer line;
+
+    // Use this for initialization
+    void Start () {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.numPositions = segments + 1;
+        line.useWorldSpace = false;
+        CreatePoints();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void CreatePoints()
+    {
+        float x;
+        float y;
+        float z = 0f;
+
+        float angle = 0f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle);
+            y = Mathf.Cos(Mathf.Deg2Rad * angle);
+            z = 5;
+            line.SetPosition(i, new Vector3(x, y, z) * radius);
+            angle += (360f / segments);
+        }
+    }
+}
+
