e132addc1834f1fde627d355fa0b5af646659a26
U-LAPTOP-MATTHEW\Matthew <h4v9a@ugrad.cs.ubc.ca>
2017-01-21 20:14:08 -0800

    created a pillar object that spawns over certain time intervals

---
+++ Pulse/Assets/Pillar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillar : MonoBehaviour {
+
+    public float spawnDelay;
+    public GameObject pillar;
+    Queue<GameObject> activePillars = new Queue<GameObject>();
+
+    // Use this for initialization
+    void Start() {
+        InvokeRepeating("Spawn", 2, spawnDelay);
+    }
+
+
+    void Spawn() {
+        GameObject obj = (GameObject)Instantiate(pillar, new Vector3(Random.Range(-20, 20), Random.Range(-15, 15), 0), Quaternion.identity);
+        activePillars.Enqueue(obj);
+    }
+}
