572d9f438362c885b37c9776d35c746861850010
KDrab <kennydrabble1994@gmail.com>
2017-01-22 08:47:59 -0800

    ouch

--- Pulse/Assets/Scripts/Pillar.cs
+++ Pulse/Assets/Scripts/Pillar.cs
@@ -7,14 +7,17 @@ public class Pillar : MonoBehaviour {
     public float spawnDelay;
     public Color checkColor;
     public GameObject pillar;
+    public GameObject redPillar;
     public Transform wavePrefab;
+    public int spawnCount;
     Queue<GameObject> activePillars = new Queue<GameObject>();
 
     // Use this for initialization
     void Start() {
         pillar = pillar;
+        redPillar = redPillar;
         InvokeRepeating("Spawn", 2, spawnDelay);
-        checkColor = pillar.GetComponent<Color>();
+//        checkColor = pillar.GetComponent<Color>();
     }
 
     // Update is called once per frame
@@ -22,13 +25,27 @@ public class Pillar : MonoBehaviour {
     }
 
     void Spawn() {
+        spawnCount++;
         GameObject obj = Instantiate(pillar, new Vector3(Random.Range(-18, 18), Random.Range(-25, 25), 0), Quaternion.identity);
+        activePillars.Enqueue(obj);
+        if (spawnCount >= 10)
+        {
+            CancelInvoke("Spawn");
+            InvokeRepeating("nextSpawn", 2, spawnDelay);
+        }
+
+
+    }
+
+    void nextSpawn()
+    {
+        GameObject obj = Instantiate(redPillar, new Vector3(Random.Range(-18, 18), Random.Range(-25, 25), 0), Quaternion.identity);
         activePillars.Enqueue(obj);
     }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.GetComponent<LineRenderer>().startColor == checkColor)
+  //      if (coll.gameObject.GetComponent<LineRenderer>().startColor == checkColor)
             Destroy(gameObject);
     }
 }
